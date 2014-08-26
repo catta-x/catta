f97dd32f0fdffd8570b711697b91f7af0342d96c
Sven M. Hallberg <pesco@khjk.org>
2014-08-26 20:50:46 +0200

    oh, we need the machine field, not nodename. also some gitignores

--- src/compat/windows/wincompat.c
+++ src/compat/windows/wincompat.c
@@ -3,12 +3,32 @@
 
 int uname(struct utsname *buf)
 {
+    SYSTEM_INFO si;
+    const char *arch = "unknown";
+
     memset(buf, 0, sizeof(struct utsname));
+
+    // operating system
     strncpy(buf->sysname, "Windows", sizeof(buf->sysname)-1);
+    strncpy(buf->release, "unknown", sizeof(buf->sysname)-1);   // we don't need it
+    strncpy(buf->version, "unknown", sizeof(buf->sysname)-1);   // we don't need it
+
+    // computer (node) name
     if(GetComputerName(buf->nodename, sizeof(buf->nodename)-1) == 0) {
         errno = EFAULT;
         return -1;
     }
+
+    // hardware type
+    GetSystemInfo(&si);
+    switch(si.wProcessorArchitecture) {
+        case PROCESSOR_ARCHITECTURE_AMD64: arch = "amd64"; break;
+        case PROCESSOR_ARCHITECTURE_ARM:   arch = "arm";   break;
+        case PROCESSOR_ARCHITECTURE_IA64:  arch = "ia64";  break;
+        case PROCESSOR_ARCHITECTURE_INTEL: arch = "x86";   break;
+        default: arch = "unknown";
+    }
+    strncpy(buf->machine, arch, sizeof(buf->machine)-1);
 
     return 0;
 }
